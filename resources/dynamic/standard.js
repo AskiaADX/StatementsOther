b70b648e641cd49ef3c30fa8cdfb28723d4eeba6
Michael Long <michael.long35@gmail.com>
2015-04-15 17:02:42 +0100

    Initial Commit, version 1.0.12

---
+++ resources/dynamic/standard.js
@@ -0,0 +1,38 @@
+﻿/* standard.js */
+$(window).load(function() {
+	$('#adc_{%= CurrentADC.InstanceId %}').adcStatementsOther({
+		target : 'jsObj{%= CurrentADC.InstanceId%}',
+		width : 400,
+		isMultiple: {%= (CurrentQuestion.Type = "multiple") %},
+		controlWidth : '{%= CurrentADC.PropValue("controlWidth") %}',
+		columns: {%= CurrentADC.PropValue("columns") %},
+		maxWidth : '{%= CurrentADC.PropValue("maxWidth") %}',
+		maxImageWidth : '{%= CurrentADC.PropValue("maxImageWidth") %}',
+		maxImageHeight : '{%= CurrentADC.PropValue("maxImageHeight") %}',
+		forceImageSize : '{%= CurrentADC.PropValue("forceImageSize") %}',
+		autoForward: {%= (CurrentADC.PropValue("autoForward") = "1") %},
+		animate: {%= (CurrentADC.PropValue("animateResponses") = "1") %},
+		animationSpeed: '{%= CurrentADC.PropValue("animationSpeed") %}',
+		numberNS: {%= CurrentADC.PropValue("numberNS") %},
+		useRange: {%= (CurrentADC.PropValue("useRange") = "1") %},
+		responseTextPadding : '{%= CurrentADC.PropValue("responseTextPadding") %}',
+		responseTextLineHeight : '{%= CurrentADC.PropValue("responseTextLineHeight") %}',
+		showResponseHoverColour: {%= (CurrentADC.PropValue("showResponseHoverColour") = "1") %},
+		showResponseHoverFontColour: {%= (CurrentADC.PropValue("showResponseHoverFontColour") = "1") %},
+		showResponseHoverBorder: {%= (CurrentADC.PropValue("showResponseHoverBorder") = "1") %},
+		controlAlign : '{%= CurrentADC.PropValue("controlAlign") %}',
+		otherRID : '{%= CurrentADC.PropValue("otherRID") %}',
+		otherQID : '{%= CurrentADC.PropValue("otherQID") %}',
+		rangeGradientDirection : '{%= CurrentADC.PropValue("rangeGradientDirection") %}',
+		{% IF CurrentADC.PropValue("useRange") = "1" Then %}
+			range: '{%= CurrentADC.PropValue("responseColourPrimary") %};{%= CurrentADC.PropValue("responseColourSecondary") %};{%= CurrentADC.PropValue("responseColourRangePrimary") %};{%= CurrentADC.PropValue("responseColourRangeSecondary") %}',
+		{% EndIF %}
+		items : [
+			{% IF CurrentQuestion.Type = "single" Then %}
+				{%:= CurrentADC.GetContent("dynamic/standard_single.js").ToText()%}
+			{% ElseIf CurrentQuestion.Type = "multiple" Then %}
+				{%:= CurrentADC.GetContent("dynamic/standard_multiple.js").ToText()%}
+			{% EndIF %}
+		]
+	});
+});
